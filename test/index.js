c79bf34ec6bb3cc2f180b9f6f66d9f9414525d84
Michael A. Cook <mcook0775@gmail.com>
2021-03-14 04:12:36 -0400

    Remove console.log

--- test/index.js
+++ test/index.js
@@ -3,6 +3,5 @@ const path = require("path")
 
 // programmatically require and call tests
 fs.readdirSync(__dirname + "/lib").forEach((file) => {
-  console.log(file)
   require(path.resolve(__dirname, "lib", file))()
 })
