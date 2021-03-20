88fdd4cdcf66f8e8272a2e92daf61bbe2a31f97a
Michael A. Cook <mcook0775@gmail.com>
2021-03-20 15:26:37 -0400

    Fix port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import path from "path"
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
