468b839bccb52b8e2aa69b4b84cd353095d9c77a
Michael A. Cook <mcook0775@gmail.com>
2021-03-23 19:27:41 -0400

    Make more expressive

--- test/index.js
+++ test/index.js
@@ -2,11 +2,11 @@ import fs from "fs"
 import path from "path"
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
-// programmatically require and call tests
+// programmatically import and call tests
 fs.readdirSync(__dirname + "/lib").forEach((file) => {
   import(path.resolve(__dirname, "lib", file))
-    .then((func) => {
-      func()
+    .then((test) => {
+      test()
     })
     .catch((err) => {
       console.log(err)
